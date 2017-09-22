2228db9f2e259c7f09fca9de4db99cf318c20bbd
James McCormack <thejamesmccormack@gmail.com>
2017-09-22 16:12:36 -0700

    temp changes

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,5 @@
 const sql = require('./../taskCommands.js');
+// might not need the pg-promise require here?
 const pgp = require('pg-promise');
 const createTask = require('./../taskCommands.js');
 const taskData = require('./fixtures/tasks.json');
@@ -18,6 +19,7 @@ const resetTaskIDs = function () {
   });
 };
 
+// you are problem function. not reading createTask correctly.
 const insertTaskData = function () {
   return Promise.all(
     taskData.map((task) => {
